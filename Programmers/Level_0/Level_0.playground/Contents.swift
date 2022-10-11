b2667d96d4277425c94768e68fdffcda5970f098
chuyeonhak <=>
2022-10-11 14:30:51 +0900

    Feat: 몫 구하기

--- Programmers/Level_0/Level_0.playground/Contents.swift
+++ Programmers/Level_0/Level_0.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+
+//두 수의 곱
+//문제 설명
+//정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+//
+//제한사항
+//0 ≤ num1 ≤ 100
+//0 ≤ num2 ≤ 100
+ 
+
+//func solution(_ num1:Int, _ num2:Int) -> Int {
+//    return num1 * num2
+//}
+//
+//solution(3, 4)
+//solution(27, 19)
+
+//몫 구하기
+//문제 설명
+//정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+//
+//제한사항
+//0 < num1 ≤ 100
+//0 < num2 ≤ 100
+
+//func solution(_ num1:Int, _ num2:Int) -> Int { num1 / num2 }
+//
+//solution(10, 5)
+//solution(7, 2)
