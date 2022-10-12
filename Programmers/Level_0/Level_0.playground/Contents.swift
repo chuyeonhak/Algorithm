dfea4183db0485ef0d93429ead9ccd42a620a22d
chuyeonhak <=>
2022-10-12 18:38:50 +0900

    피자 나눠 먹기(2)

--- Programmers/Level_0/Level_0.playground/Contents.swift
+++ Programmers/Level_0/Level_0.playground/Contents.swift
@@ -1383,21 +1383,42 @@ import UIKit
 //1 ≤ numbers의 길이 ≤ 100
 //direction은 "left" 와 "right" 둘 중 하나입니다.
 
-func solution(_ numbers:[Int], _ direction:String) -> [Int] {
-    var tempArr = numbers
-    switch direction {
-    case "right":
-        tempArr.insert(numbers.last!, at: 0)
-        return tempArr.dropLast()
-    case "left":
-        tempArr.append(numbers.first!)
-        return Array(tempArr.dropFirst())
-    default: return []
+//func solution(_ numbers:[Int], _ direction:String) -> [Int] {
+//    var tempArr = numbers
+//    switch direction {
+//    case "right":
+//        tempArr.insert(numbers.last!, at: 0)
+//        return tempArr.dropLast()
+//    case "left":
+//        tempArr.append(numbers.first!)
+//        return Array(tempArr.dropFirst())
+//    default: return []
+//    }
+//}
+//
+//solution([1, 2, 3], "right")
+//solution([4, 455, 6, 4, -1, 45, 6], "left")
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//피자 나눠 먹기 (2)
+//문제 설명
+//머쓱이네 피자가게는 피자를 여섯 조각으로 잘라 줍니다. 피자를 나눠먹을 사람의 수 n이 매개변수로 주어질 때, n명이 주문한 피자를 남기지 않고 모두 같은 수의 피자 조각을 먹어야 한다면 최소 몇 판을 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
+//
+//제한사항
+//1 ≤ n ≤ 100
+func solution(_ n:Int) -> Int {
+    var min = 1
+    
+    while (min * n) % 6 != 0 {
+        min += 1
     }
+    return (min * n) / 6
 }
 
-solution([1, 2, 3], "right")
-solution([4, 455, 6, 4, -1, 45, 6], "left")
+solution(6)
+solution(10)
+solution(4)
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
