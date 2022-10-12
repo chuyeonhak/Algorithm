3c5da55db6d16e98960cfbfec36510115d9b1c8e
chuyeonhak <=>
2022-10-12 19:04:21 +0900

    A로 B 만들기

--- Programmers/Level_0/Level_0.playground/Contents.swift
+++ Programmers/Level_0/Level_0.playground/Contents.swift
@@ -1407,18 +1407,30 @@ import UIKit
 //
 //제한사항
 //1 ≤ n ≤ 100
-func solution(_ n:Int) -> Int {
-    var min = 1
-    
-    while (min * n) % 6 != 0 {
-        min += 1
-    }
-    return (min * n) / 6
-}
-
-solution(6)
-solution(10)
-solution(4)
+//func solution(_ n:Int) -> Int {
+//    var min = 1
+//
+//    while (min * n) % 6 != 0 {
+//        min += 1
+//    }
+//    return (min * n) / 6
+//}
+//
+//solution(6)
+//solution(10)
+//solution(4)
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//A로 B 만들기
+//문제 설명
+//문자열 before와 after가 매개변수로 주어질 때, before의 순서를 바꾸어 after를 만들 수 있으면 1을, 만들 수 없으면 0을 return 하도록 solution 함수를 완성해보세요.
+//
+//제한사항
+//0 < before의 길이 == after의 길이 < 1,000
+//before와 after는 모두 소문자로 이루어져 있습니다.
+
+func solution(_ before:String, _ after:String) -> Int { return Dictionary(grouping: before) { $0 } == Dictionary(grouping: after) { $0 } ? 1: 0 }
+
+solution("olleh", "hello")
+solution("allpe", "apple")
