4a979143be6e1f4bd4ec8d42528cedb845ef2445
chuyeonhak <=>
2022-10-13 08:23:44 +0900

    다음에 올 숫자

--- Programmers/Level_0/Level_0.playground/Contents.swift
+++ Programmers/Level_0/Level_0.playground/Contents.swift
@@ -1599,19 +1599,42 @@ import UIKit
 //1 ≤ n ≤ 100
 //가장 가까운 수가 여러 개일 경우 더 작은 수를 return 합니다.
 
-func solution(_ array:[Int], _ n:Int) -> Int {
-    guard array.count > 1 else { return array[0]}
-    let sortedArray = array.sorted { abs($0 - n) < abs($1 - n) }
-    
-    return abs(sortedArray[0] - n) == abs(sortedArray[1] - n) ? min(sortedArray[0], sortedArray[1]): sortedArray[0]
+//func solution(_ array:[Int], _ n:Int) -> Int {
+//    guard array.count > 1 else { return array[0]}
+//    let sortedArray = array.sorted { abs($0 - n) < abs($1 - n) }
+//
+//    return abs(sortedArray[0] - n) == abs(sortedArray[1] - n) ? min(sortedArray[0], sortedArray[1]): sortedArray[0]
+//}
+//
+//func solution(_ array:[Int], _ n:Int) -> Int {
+//    return array.min(by: { (abs($0 - n), $0) < (abs($1 - n), $1) })!
+//}
+//
+//solution([3, 10, 28], 20)
+//solution([10, 11, 12], 13)
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//다음에 올 숫자
+//문제 설명
+//등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
+//
+//제한사항
+//2 < common의 길이 < 1,000
+//-1,000 < common의 원소 < 2,000
+//등차수열 혹은 등비수열이 아닌 경우는 없습니다.
+//공비가 0인 경우는 없습니다.
+
+func solution(_ common:[Int]) -> Int {
+    if common[0] - common[1] == common[1] - common [2] {
+        return common.last! + common[1] - common[0]
+    } else {
+        return common.last! * common[1] / common[0]
+    }
 }
 
-func solution(_ array:[Int], _ n:Int) -> Int {
-    return array.min(by: { (abs($0 - n), $0) < (abs($1 - n), $1) })!
-}
-
-solution([3, 10, 28], 20)
-solution([10, 11, 12], 13)
+solution([1, 2 ,3, 4])
+solution([2, 4, 8])
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
