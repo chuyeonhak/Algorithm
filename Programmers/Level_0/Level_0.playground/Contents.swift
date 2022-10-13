7f7a396aa6983ad7b5400eb527bd054298d5d5e1
chuyeonhak <=>
2022-10-13 13:55:35 +0900

    외계어 사전

--- Programmers/Level_0/Level_0.playground/Contents.swift
+++ Programmers/Level_0/Level_0.playground/Contents.swift
@@ -1671,23 +1671,60 @@ import UIKit
 //모든 숫자를 지우는 경우는 주어지지 않습니다.
 //지울 숫자가 없는 상태에서 "Z"는 무시합니다.
 
-func solution(_ s:String) -> Int {
-    var tempArr = s.components(separatedBy: " ")
-    var cnt = 0
-    
-    for i in tempArr.enumerated() {
-        if i.element == "Z" {
-            tempArr.removeSubrange((i.offset - cnt - 1)...(i.offset - cnt))
-            cnt += 2
-        }
-    }
-    
-    return tempArr.reduce(0) { $0 + Int($1)! }
-}
-
-solution("1 2 Z 3")
-solution("10 20 30 40")
-solution("10 Z 20 Z 1")
+//func solution(_ s:String) -> Int {
+//    var tempArr = s.components(separatedBy: " ")
+//    var cnt = 0
+//
+//    for i in tempArr.enumerated() {
+//        if i.element == "Z" {
+//            tempArr.removeSubrange((i.offset - cnt - 1)...(i.offset - cnt))
+//            cnt += 2
+//        }
+//    }
+//
+//    return tempArr.reduce(0) { $0 + Int($1)! }
+//}
+//
+//func solution(_ s: String) -> Int {
+//    var stack = [Int]()
+//
+//    for w in s.components(separatedBy: [" "]) {
+//        if w == "Z" {
+//            stack.popLast()
+//        } else {
+//            stack.append(Int(w)!)
+//        }
+//    }
+//    return stack.reduce(0, +)
+//}
+//
+//solution("1 2 Z 3")
+//solution("10 20 30 40")
+//solution("10 Z 20 Z 1")
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//외계어 사전
+//문제 설명
+//PROGRAMMERS-962 행성에 불시착한 우주비행사 머쓱이는 외계행성의 언어를 공부하려고 합니다. 알파벳이 담긴 배열 spell과 외계어 사전 dic이 매개변수로 주어집니다. spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 1, 존재하지 않는다면 2를 return하도록 solution 함수를 완성해주세요.
+//
+//제한사항
+//spell과 dic의 원소는 알파벳 소문자로만 이루어져있습니다.
+//2 ≤ spell의 크기 ≤ 10
+//spell의 원소의 길이는 1입니다.
+//1 ≤ dic의 크기 ≤ 10
+//1 ≤ dic의 원소의 길이 ≤ 10
+//spell의 원소를 모두 사용해 단어를 만들어야 합니다.
+//spell의 원소를 모두 사용해 만들 수 있는 단어는 dic에 두 개 이상 존재하지 않습니다.
+//dic과 spell 모두 중복된 원소를 갖지 않습니다
+
+func solution(_ spell:[String], _ dic:[String]) -> Int { dic.filter { Set($0) == Set(spell.map { Character($0) }) }.isEmpty ? 2 : 1 }
+
+solution(["p", "o", "s"], ["sod", "eocd", "qixm", "adio", "soo"])
+solution(["z", "d", "x"], ["def", "dww", "dzx", "loveaw"])
+solution(["s", "o", "m", "d"], ["moos", "dzx", "smm", "sunmmo", "som"])
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
