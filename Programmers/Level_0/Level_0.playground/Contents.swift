2297317990e70d80a430d94f35183d29ca94d503
chuyeonhak <=>
2022-10-14 08:19:34 +0900

    옹알이

--- Programmers/Level_0/Level_0.playground/Contents.swift
+++ Programmers/Level_0/Level_0.playground/Contents.swift
@@ -1809,31 +1809,65 @@ import UIKit
 //-20,000 ≤ Z ≤ 20,000
 //[연산자]는 + 와 - 중 하나입니다.
 
-func solution(_ quiz:[String]) -> [String] {
-    let strArr = quiz.map { $0.components(separatedBy: " ") }
-    var result: [String] = []
-    strArr.forEach {
-        switch $0[1] {
-        case "-":
-            result.append(Int($0[0])! - Int($0[2])! == Int($0[4])! ? "O": "X")
-        case "+":
-            result.append(Int($0[0])! + Int($0[2])! == Int($0[4])! ? "O": "X")
-        default: print("wow")
+//func solution(_ quiz:[String]) -> [String] {
+//    let strArr = quiz.map { $0.components(separatedBy: " ") }
+//    var result: [String] = []
+//    strArr.forEach {
+//        switch $0[1] {
+//        case "-":
+//            result.append(Int($0[0])! - Int($0[2])! == Int($0[4])! ? "O": "X")
+//        case "+":
+//            result.append(Int($0[0])! + Int($0[2])! == Int($0[4])! ? "O": "X")
+//        default: print("wow")
+//        }
+//    }
+//
+//    return result
+//}
+//
+//func solution(_ quiz: [String]) -> [String] {
+//    return quiz.map {
+//        let arr = $0.split(separator: " ")
+//        return arr[1] == "+" && Int(arr[0])! + Int(arr[2])! == Int(arr[4])! || Int(arr[0])! - Int(arr[2])! == Int(arr[4]) ? "O" : "X"
+//    }
+//}
+//
+//solution(["3 - 4 = -3", "5 + 6 = 11"])
+//solution(["19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"])
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//옹알이
+//문제 설명
+//머쓱이는 태어난 지 11개월 된 조카를 돌보고 있습니다. 조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음과 네 가지 발음을 조합해서 만들 수 있는 발음밖에 하지 못하고 연속해서 같은 발음을 하는 것을 어려워합니다. 문자열 배열 babbling이 매개변수로 주어질 때, 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
+//
+//제한사항
+//1 ≤ babbling의 길이 ≤ 10
+//1 ≤ babbling[i]의 길이 ≤ 30
+//문자열은 알파벳 소문자로만 이루어져 있습니다.
+
+func solution(_ babbling:[String]) -> Int {
+    let tempArr1 = ["aya", "ye", "woo", "ma"],
+        tempArr2 = ["ayaaya", "yeye", "woowoo", "mama"]
+    var result = 0
+    for babble in babbling {
+        var copy = babble
+        for i in tempArr2 {
+            copy = copy.replacingOccurrences(of: i, with: "x")
         }
+        
+        for j in tempArr1 {
+            copy = copy.replacingOccurrences(of: j, with: "")
+        }
+        
+        if copy.isEmpty {
+            result += 1
+        }
+        
     }
-    
     return result
 }
 
-func solution(_ quiz: [String]) -> [String] {
-    return quiz.map {
-        let arr = $0.split(separator: " ")
-        return arr[1] == "+" && Int(arr[0])! + Int(arr[2])! == Int(arr[4])! || Int(arr[0])! - Int(arr[2])! == Int(arr[4]) ? "O" : "X"
-    }
-}
+solution(["aya", "yee", "u", "maa"])
+solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"])
 
-solution(["3 - 4 = -3", "5 + 6 = 11"])
-solution(["19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"])
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
