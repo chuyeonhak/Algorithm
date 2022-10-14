f5e1f4367deb682b9ea8351f0be789e397a150f1
chuyeonhak <=>
2022-10-14 16:19:18 +0900

    직사각형 넓이 구하기

--- Programmers/Level_0/Level_0.playground/Contents.swift
+++ Programmers/Level_0/Level_0.playground/Contents.swift
@@ -2077,31 +2077,66 @@ import UIKit
 //score의 원소 길이는 2입니다.
 //score는 중복된 원소를 갖지 않습니다.
 
-func solution(_ score:[[Int]]) -> [Int] {
-    let avgArray = score.map { ($0[0] + $0[1]) }
-    var result = Array(repeating: 1, count: score.count)
+//func solution(_ score:[[Int]]) -> [Int] {
+//    let avgArray = score.map { ($0[0] + $0[1]) }
+//    var result = Array(repeating: 1, count: score.count)
+//
+//    for i in avgArray.enumerated() {
+//        for j in avgArray {
+//            if i.element != j && i.element < j {
+//                result[i.offset] = result[i.offset] + 1
+//            }
+//        }
+//    }
+//
+//    return result
+//}
+//
+//func solution(_ score:[[Int]]) -> [Int] {
+//    let average = score.map { (Double($0[0] + $0[1]) / 2.0) }
+//    let sortedAverage = average.sorted(by: >)
+//    return average.map { (sortedAverage.firstIndex(of: $0) ?? 0) + 1 }
+//}
+//
+//func solution(_ score: [[Int]]) -> [Int] { score.map { Double($0.reduce(0, +)) / Double($0.count) }.map { score.map { Double($0.reduce(0, +)) / Double($0.count) }.sorted(by: >).firstIndex(of: $0)! + 1 } }
+//
+//solution([[80, 70], [90, 50], [40, 70], [50, 80]])
+//solution([[80, 70], [70, 80], [30, 50], [90, 100], [100, 90], [100, 100], [10, 30]])
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    for i in avgArray.enumerated() {
-        for j in avgArray {
-            if i.element != j && i.element < j {
-                result[i.offset] = result[i.offset] + 1
-            }
-        }
-    }
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//직사각형 넓이 구하기
+//문제 설명
+//2차원 좌표 평면에 변이 축과 평행한 직사각형이 있습니다. 직사각형 네 꼭짓점의 좌표 [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]가 담겨있는 배열 dots가 매개변수로 주어질 때, 직사각형의 넓이를 return 하도록 solution 함수를 완성해보세요.
+//
+//제한사항
+//dots의 길이 = 4
+//dots의 원소의 길이 = 2
+//-256 < dots[i]의 원소 < 256
+//잘못된 입력은 주어지지 않습니다.
 
-    return result
+//func solution(_ dots:[[Int]]) -> Int {
+//    var result: [Double] = []
+//    (1..<4).forEach {
+//        result.append(getLenght(dots[0], dots[$0]))
+//    }
+//
+//    return Int(result.sorted()[0] * result.sorted()[1])
+//}
+
+private func getLenght(_ dot1: [Int], _ dot2: [Int]) -> Double { sqrt(pow(Double(dot1[0] - dot2[0]), 2) + pow(Double(dot1[1] - dot2[1]), 2)) }
+
+func solution(_ dots:[[Int]]) -> Int {
+    let a = [dots[1][0] - dots[0][0], dots[1][1] - dots[0][1]]
+    print(dots[1][0])
+    let b = [dots[2][0] - dots[0][0], dots[2][1] - dots[0][1]]
+
+    return abs(a[0]*b[1] - a[1]*b[0])
 }
 
-func solution(_ score:[[Int]]) -> [Int] {
-    let average = score.map { (Double($0[0] + $0[1]) / 2.0) }
-    let sortedAverage = average.sorted(by: >)
-    return average.map { (sortedAverage.firstIndex(of: $0) ?? 0) + 1 }
-}
-
-func solution(_ score: [[Int]]) -> [Int] { score.map { Double($0.reduce(0, +)) / Double($0.count) }.map { score.map { Double($0.reduce(0, +)) / Double($0.count) }.sorted(by: >).firstIndex(of: $0)! + 1 } }
-
-solution([[80, 70], [90, 50], [40, 70], [50, 80]])
-solution([[80, 70], [70, 80], [30, 50], [90, 100], [100, 90], [100, 100], [10, 30]])
+solution([[1, 1], [2, 1],
+          [2, 2], [1, 2]])
+solution([[-1, -1], [1, 1], [1, -1], [-1, 1]])
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
