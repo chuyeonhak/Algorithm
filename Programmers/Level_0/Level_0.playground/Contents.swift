224fb4b4172047acf17ac785b1fd9b5fa2f0fc5b
chuyeonhak <=>
2022-10-14 17:55:19 +0900

    소인수분해

--- Programmers/Level_0/Level_0.playground/Contents.swift
+++ Programmers/Level_0/Level_0.playground/Contents.swift
@@ -2148,32 +2148,54 @@ import UIKit
 //0 < A의 길이 = B의 길이 < 100
 //A, B는 알파벳 소문자로 이루어져 있습니다.
 
-import Foundation
+//func solution(_ A:String, _ B:String) -> Int {
+//    var copy = B.map { $0 }
+//    var result = 0
+//
+//    while true {
+//        if String(copy) == A || result == A.count { break }
+//        result += 1
+//
+//        let first = copy[0]
+//        copy.removeFirst()
+//        copy.append(first)
+//
+//    }
+//
+//    return result == A.count ? -1 : result
+//}
+//
+//func solution(_ A:String, _ B:String) -> Int {
+//    let arr = (A+A).components(separatedBy: B))
+//    return arr.count == 1 ? -1 : arr[1].count
+//}
+//
+//solution("hello", "ohell")
+//solution("apple", "elppa")
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-func solution(_ A:String, _ B:String) -> Int {
-    var copy = B.map { $0 }
-    var result = 0
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//소인수분해
+//문제 설명
+//소인수분해란 어떤 수를 소수들의 곱으로 표현하는 것입니다. 예를 들어 12를 소인수 분해하면 2 * 2 * 3 으로 나타낼 수 있습니다. 따라서 12의 소인수는 2와 3입니다. 자연수 n이 매개변수로 주어질 때 n의 소인수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+//제한사항
+//2 ≤ n ≤ 10,000
 
-    while true {
-        if String(copy) == A || result == A.count { break }
-        result += 1
-
-        let first = copy[0]
-        copy.removeFirst()
-        copy.append(first)
-
+func solution(_ n:Int) -> [Int] {
+    var result: [Int] = []
+    
+    for i in (2...n) {
+        if n % i == 0 && !result.contains(where: { i % $0 == 0}) {
+            result.append(i)
+        }
     }
-
-    return result == A.count ? -1 : result
+    
+    return result
 }
 
-func solution(_ A:String, _ B:String) -> Int {
-    let arr = (A+A).components(separatedBy: B))
-    return arr.count == 1 ? -1 : arr[1].count
-}
-
-solution("hello", "ohell")
-solution("apple", "elppa")
+solution(12)
+solution(17)
+solution(420)
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
