f620b5613c6e5f25f82dca65df0702992de5b140
chuyeonhak <=>
2022-10-17 09:39:47 +0900

    1단계 시작

--- Programmers/Level_0/Level_0.playground/Contents.swift
+++ Programmers/Level_0/Level_0.playground/Contents.swift
@@ -2320,19 +2320,19 @@ import UIKit
 //chicken은 정수입니다.
 //0 ≤ chicken ≤ 1,000,000
 
-func solution(_ chicken:Int) -> Int {
-    var copy = chicken
-    var service = 0
-    
-    while copy / 10 != 0 {
-        service += copy / 10
-        copy = (copy / 10) + copy % 10
-    }
-    return service
-}
-
-solution(100)
-solution(1081)
+//func solution(_ chicken:Int) -> Int {
+//    var copy = chicken
+//    var service = 0
+//
+//    while copy / 10 != 0 {
+//        service += copy / 10
+//        copy = (copy / 10) + copy % 10
+//    }
+//    return service
+//}
+//
+//solution(100)
+//solution(1081)
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -2349,19 +2349,64 @@ solution(1081)
 //sides의 길이는 2입니다.
 //1 ≤ sides의 원소 ≤ 1,000
 
-func solution(_ sides:[Int]) -> Int {
-    let sortedSides = sides.sorted()
+//func solution(_ sides:[Int]) -> Int {
+//    let sortedSides = sides.sorted()
+//    var result = 0
+//
+//    ((sortedSides[1] - sortedSides[0] + 1)...(sortedSides[1])).forEach { _ in result += 1 }
+//    ((sortedSides[1] + 1)..<(sides[0] + sides[1])).forEach { _ in result += 1 }
+//
+//    return result
+//}
+//
+//solution([1, 2])
+//solution([3, 6])
+//solution([11, 7])
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//겹치는 선분의 길이
+//문제 설명
+//빨간색, 초록색, 파란색 선분이 x축 위에 있습니다. 세 선분의 x좌표 시작과 끝이 [[start, end], [start, end], [start, end]] 형태로 들어있는 2차원 배열 lines가 매개변수로 주어질 때, 두 개 이상의 선분이 겹치는 부분의 길이를return 하도록 solution 함수를 완성해보세요.
+//
+//lines가 [[0, 2], [-3, -1], [-2, 1]]일 때 그림으로 나타내면 다음과 같습니다.
+//
+//선분이 두 개 이상 겹친 곳은 [-2, -1], [0, 1]로 2만큼 겹쳐있습니다.
+//
+//제한사항
+//-100 < lines의 원소 < 100
+//lines의 길이 = 3
+//lines의 원소의 길이 = 2
+//모든 선분은 길이가 1 이상입니다.
+//lines의 원소는 [a, b] 형태이며, a, b는 각각 양 끝점 중 하나입니다.
+//-100 < a, b < 100
+
+func solution(_ lines:[[Int]]) -> Int {
+    var tempDic: [Int: Int] = [:]
     var result = 0
+    var key = 0
+    for line in lines {
+        for i in (line.min()!...line.max()!) {
+            let value = tempDic[i] ?? 0
+            tempDic.updateValue(value + 1, forKey: i)
+        }
+    }
     
-    ((sortedSides[1] - sortedSides[0] + 1)...(sortedSides[1])).forEach { _ in result += 1 }
-    ((sortedSides[1] + 1)..<(sides[0] + sides[1])).forEach { _ in result += 1 }
+    let sortedDic = tempDic.sorted(by: <).filter { $0.value >= 2 }.map { $0.key }
+    key = sortedDic.first!
+    sortedDic.map { $0 - sortedDic.first! }.forEach {
+        if $0 == key + 1 {
+            result += 1
+            key = $0
+        } else {
+            key = $0
+        }
+    }
     
     return result
 }
 
-solution([1, 2])
-solution([3, 6])
-solution([11, 7])
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+solution([[0, 1], [2, 5], [3, 9]])
+solution([[1, -1], [1, 3], [9, 3]])
+solution([[0, 5], [3, 9], [1, 10]])
+solution([[-1, 0], [0, 1], [1, 2]])
