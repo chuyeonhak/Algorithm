5787a6bc2e53c1e86c7c8d76b96679e5eed9af88
chuyeonhak <=>
2022-10-17 18:07:01 +0900

    서울에서 김서방 찾기

--- Programmers/Level_1/Level_1.playground/Contents.swift
+++ Programmers/Level_1/Level_1.playground/Contents.swift
@@ -283,21 +283,36 @@ import UIKit
 //s가 "zero" 또는 "0"으로 시작하는 경우는 주어지지 않습니다.
 //return 값이 1 이상 2,000,000,000 이하의 정수가 되는 올바른 입력만 s로 주어집니다.
 
-func solution(_ s:String) -> Int {
-    let numberTupleList = [("zero", 0), ("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)]
-    var result = s
+//func solution(_ s:String) -> Int {
+//    let numberTupleList = [("zero", 0), ("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)]
+//    var result = s
+//
+//    for i in numberTupleList {
+//        result = result.replacingOccurrences(of: i.0, with: String(i.1))
+//    }
+//
+//    return Int(result)!
+//}
+//
+//solution("one4seveneight")
+//solution("23four5six7")
+//solution("2three45sixseven")
+//solution("123")
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    for i in numberTupleList {
-        result = result.replacingOccurrences(of: i.0, with: String(i.1))
-    }
-    
-    return Int(result)!
-}
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//서울에서 김서방 찾기
+//문제 설명
+//String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아, "김서방은 x에 있다"는 String을 반환하는 함수, solution을 완성하세요. seoul에 "Kim"은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
+//
+//제한 사항
+//seoul은 길이 1 이상, 1000 이하인 배열입니다.
+//seoul의 원소는 길이 1 이상, 20 이하인 문자열입니다.
+//"Kim"은 반드시 seoul 안에 포함되어 있습니다.
 
-solution("one4seveneight")
-solution("23four5six7")
-solution("2three45sixseven")
-solution("123")
+func solution(_ seoul:[String]) -> String { "김서방은 \(seoul.firstIndex(of: "Kim")!)에 있다" }
+
+solution(["Jane", "Kim"])
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
