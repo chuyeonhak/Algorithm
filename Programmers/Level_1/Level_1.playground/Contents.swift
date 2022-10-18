f31c336391bc2f325149455007bd636e266fd812
chuyeonhak <=>
2022-10-18 14:47:36 +0900

    모의고사

--- Programmers/Level_1/Level_1.playground/Contents.swift
+++ Programmers/Level_1/Level_1.playground/Contents.swift
@@ -919,21 +919,93 @@ extension Int {
 //nums에 들어있는 숫자의 개수는 3개 이상 50개 이하입니다.
 //nums의 각 원소는 1 이상 1,000 이하의 자연수이며, 중복된 숫자가 들어있지 않습니다.
 
-func solution(_ nums:[Int]) -> Int {
-    var intArr: [Int] = []
-    for i in 0..<nums.count {
-        for j in i + 1..<nums.count {
-            for k in j + 1..<nums.count {
-                intArr.append(nums[i] + nums[j] + nums[k])
-            }
-        }
+//func solution(_ nums:[Int]) -> Int {
+//    var intArr: [Int] = []
+//    for i in 0..<nums.count {
+//        for j in i + 1..<nums.count {
+//            for k in j + 1..<nums.count {
+//                intArr.append(nums[i] + nums[j] + nums[k])
+//            }
+//        }
+//    }
+//
+//    return intArr.filter { $0.isPrime }.count
+//}
+//
+//solution([1,2,3,4])
+//solution([1,2,7,6,4])
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//모의고사
+//문제 설명
+//수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+//
+//1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+//2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+//3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+//
+//1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+//
+//제한 조건
+//시험은 최대 10,000 문제로 구성되어있습니다.
+//문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
+//가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
+
+//func solution(_ answers:[Int]) -> [Int] {
+//    let stud1 = "12345"
+//    let stud2 = "21232425"
+//    let stud3 = "3311224455"
+//    var result1: [Int] = []
+//    var result2: [Int] = []
+//    var result3: [Int] = []
+//    var answer: [Int] = []
+//    for _ in 0..<(answers.count / stud1.count) + 1 {
+//        result1 += stud1.map { Int(String(repeating: $0, count: 1))! }
+//    }
+//
+//    for _ in 0..<(answers.count / stud2.count) + 1 {
+//        result2 += stud2.map { Int(String(repeating: $0, count: 1))! }
+//    }
+//
+//    for _ in 0..<(answers.count / stud3.count) + 1 {
+//        result3 += stud3.map { Int(String(repeating: $0, count: 1))! }
+//    }
+//
+//    let count1 = (zip(result1, answers).filter { $0.0 == $0.1 }.count, 1)
+//    let count2 = (zip(result2, answers).filter { $0.0 == $0.1 }.count, 2)
+//    let count3 = (zip(result3, answers).filter { $0.0 == $0.1 }.count, 3)
+//
+//    let max = [count1.0, count2.0, count3.0].max()!
+//
+//    [count1, count2, count3].forEach {
+//        if $0.0 == max {
+//            answer.append($0.1)
+//        }
+//    }
+//
+//    return answer
+//}
+
+func solution(_ answers:[Int]) -> [Int] {
+    let answer = (
+        a: [1, 2, 3, 4, 5], // index % 5
+        b: [2, 1, 2, 3, 2, 4, 2, 5], // index % 8
+        c: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] // index % 10
+    )
+    var point = [1:0, 2:0, 3:0]
+
+    for (i, v) in answers.enumerated() {
+        if v == answer.a[i % 5] { point[1] = point[1]! + 1 }
+        if v == answer.b[i % 8] { point[2] = point[2]! + 1 }
+        if v == answer.c[i % 10] { point[3] = point[3]! + 1 }
     }
-    
-    return intArr.filter { $0.isPrime }.count
+
+    return point.sorted{ $0.key < $1.key }.filter{ $0.value == point.values.max() }.map{ $0.key }
 }
 
-solution([1,2,3,4])
-solution([1,2,7,6,4])
+solution([1,2,3,4,5])
+solution([1,3,2,4,2])
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
