24bfca46ff952b510d3f593a5945574f49edee17
chuyeonhak <chuyeonhak@nate.com>
2022-10-21 01:07:14 +0900

    숫자 짝꿍 푸는 중

--- Programmers/Level_1/Level_1.playground/Contents.swift
+++ Programmers/Level_1/Level_1.playground/Contents.swift
@@ -1544,28 +1544,70 @@ extension Int {
 //new_id는 알파벳 대문자, 알파벳 소문자, 숫자, 특수문자로 구성되어 있습니다.
 //new_id에 나타날 수 있는 특수문자는 -_.~!@#$%^&*()=+[{]}:?,<>/ 로 한정됩니다.
 
-func solution(_ new_id:String) -> String {
-    var newId = new_id
-        .lowercased()
-        .replacingOccurrences(of: "[^a-z0-9-_.]", with: "", options: .regularExpression, range: nil)
-        .replacingOccurrences(of: #"\.{1,}"#, with: ".", options: .regularExpression, range: nil)
-    
-    if newId.hasPrefix(".") { newId.removeFirst() }
-    if newId.hasSuffix(".") { newId.removeLast() }
-    if newId.isEmpty { newId.append("a")}
-    if newId.count > 15 { newId.removeLast(newId.count - 15)}
-    if newId.last == "." { newId.removeLast() }
-    while newId.count <= 2 {
-        newId.append(newId.last!)
-    }
-    
-    return newId
-}
-solution("...!@BaT#*..y.abcdefghijklm") // "bat.y.abcdefghi"
-solution("z-+.^.")                      // "z--"
-solution("=.=")                         // "aaa"
-solution("123_.def")                    // "123_.def"
-solution("abcdefghijklmn.p")            // "abcdefghijklmn"
+//func solution(_ new_id:String) -> String {
+//    var newId = new_id
+//        .lowercased()
+//        .replacingOccurrences(of: "[^a-z0-9-_.]", with: "", options: .regularExpression, range: nil)
+//        .replacingOccurrences(of: #"\.{1,}"#, with: ".", options: .regularExpression, range: nil)
+//
+//    if newId.hasPrefix(".") { newId.removeFirst() }
+//    if newId.hasSuffix(".") { newId.removeLast() }
+//    if newId.isEmpty { newId.append("a")}
+//    if newId.count > 15 { newId.removeLast(newId.count - 15)}
+//    if newId.last == "." { newId.removeLast() }
+//    while newId.count <= 2 {
+//        newId.append(newId.last!)
+//    }
+//
+//    return newId
+//}
+//solution("...!@BaT#*..y.abcdefghijklm") // "bat.y.abcdefghi"
+//solution("z-+.^.")                      // "z--"
+//solution("=.=")                         // "aaa"
+//solution("123_.def")                    // "123_.def"
+//solution("abcdefghijklmn.p")            // "abcdefghijklmn"
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//숫자 짝꿍
+//문제 설명
+//두 정수 X, Y의 임의의 자리에서 공통으로 나타나는 정수 k(0 ≤ k ≤ 9)들을 이용하여 만들 수 있는 가장 큰 정수를 두 수의 짝꿍이라 합니다(단, 공통으로 나타나는 정수 중 서로 짝지을 수 있는 숫자만 사용합니다). X, Y의 짝꿍이 존재하지 않으면, 짝꿍은 -1입니다. X, Y의 짝꿍이 0으로만 구성되어 있다면, 짝꿍은 0입니다.
+//
+//예를 들어, X = 3403이고 Y = 13203이라면, X와 Y의 짝꿍은 X와 Y에서 공통으로 나타나는 3, 0, 3으로 만들 수 있는 가장 큰 정수인 330입니다. 다른 예시로 X = 5525이고 Y = 1255이면 X와 Y의 짝꿍은 X와 Y에서 공통으로 나타나는 2, 5, 5로 만들 수 있는 가장 큰 정수인 552입니다(X에는 5가 3개, Y에는 5가 2개 나타나므로 남는 5 한 개는 짝 지을 수 없습니다.)
+//두 정수 X, Y가 주어졌을 때, X, Y의 짝꿍을 return하는 solution 함수를 완성해주세요.
+//
+//제한사항
+//3 ≤ X, Y의 길이(자릿수) ≤ 3,000,000입니다.
+//X, Y는 0으로 시작하지 않습니다.
+//X, Y의 짝꿍은 상당히 큰 정수일 수 있으므로, 문자열로 반환합니다.
+
+func solution(_ X:String, _ Y:String) -> String {
+    var tuple: [(String.Element, Int)] = [("9", 0), ("8", 0), ("7", 0), ("6", 0), ("5", 0), ("4", 0), ("3", 0), ("2", 0), ("1", 0), ("0", 0)]
+    var copy = Y
+    var answer: String = ""
+    
+    for i in X {
+        if copy.contains(i) {
+            let index = 9 - Int(String(i))!
+            copy.remove(at: copy.firstIndex(of: i)!)
+            let value = tuple[index].1
+            tuple[index].1 = value + 1
+        }
+    }
+    tuple.forEach {
+        answer += String(repeating: $0.0, count: $0.1)
+    }
+    guard !answer.isEmpty else { return "-1" }
+    guard Int(answer) != 0 else { return "0"}
+                               
+    return answer
+}
+
+solution("100", "2345")     // "-1"
+solution("100", "203045")   // "0"
+solution("100", "123450")   // "10"
+solution("12321", "42531")  // "321"
+solution("5525", "1255")    // "552"
+solution("11111112121212121212121", "1")      // "1"
+solution("5879275893275892378989573289578923578923748932789473298429", "1437448478324789247893274892378497238947892347982384723949832749387479823748932")
+        
