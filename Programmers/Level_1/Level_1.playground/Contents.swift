e382eeb0d4e85e138baa43027caea386a88d965a
chuyeonhak <=>
2022-10-20 15:13:17 +0900

    실패율

--- Programmers/Level_1/Level_1.playground/Contents.swift
+++ Programmers/Level_1/Level_1.playground/Contents.swift
@@ -1222,24 +1222,26 @@ extension Int {
 //여벌 체육복이 있는 학생만 다른 학생에게 체육복을 빌려줄 수 있습니다.
 //여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다. 이때 이 학생은 체육복을 하나만 도난당했다고 가정하며, 남은 체육복이 하나이기에 다른 학생에게는 체육복을 빌려줄 수 없습니다.
 
-func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
-    let stupidStudent = lost.filter { reserve.contains($0) }
-    let haveNotSuit = n - lost.count
-    var realReserve = reserve.sorted(by: <)
-    var realLost = lost.sorted(by: <)
-    realReserve.removeAll { stupidStudent.contains($0) }
-    realLost.removeAll { reserve.contains($0) }
-    
-    for i in realLost {
-        if realReserve.contains(i - 1) {
-            realReserve.removeAll { $0 == i - 1 }
-        } else if realReserve.contains(i + 1) {
-            realReserve.removeAll { $0 == i + 1 }
-        }
-    }
-    
-    return haveNotSuit + reserve.count - realReserve.count
-}
+//func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
+//    let stupidStudent = lost.filter { reserve.contains($0) }
+//    let haveNotSuit = n - lost.count
+////    var realReserve = reserve.sorted(by: <).filter { !(stupidStudent.contains($0)) } 이렇게 해도 되는데 성능이 느림
+////    var realLost = lost.sorted(by: <).filter { !(reserve.contains($0)) }
+//    var realReserve = reserve.sorted(by: <)
+//    var realLost = lost.sorted(by: <)
+//    realReserve.removeAll { stupidStudent.contains($0) }
+//    realLost.removeAll { reserve.contains($0) }
+//
+//    for i in realLost {
+//        if realReserve.contains(i - 1) {
+//            realReserve.removeAll { $0 == i - 1 }
+//        } else if realReserve.contains(i + 1) {
+//            realReserve.removeAll { $0 == i + 1 }
+//        }
+//    }
+//
+//    return haveNotSuit + reserve.count - realReserve.count
+//}
 
 //solution(5, [2, 4], [1, 3, 5])
 //solution(5, [1,3], [2,4])
@@ -1250,6 +1252,52 @@ func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
 //solution(6, [3, 6], [4,5]) // 6
 //solution(7, [1, 3, 5, 7], [2, 4])
 //solution(5, [4,2], [3,5])
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//실패율
+//문제 설명
+//실패율
+//
+//슈퍼 게임 개발자 오렐리는 큰 고민에 빠졌다. 그녀가 만든 프랜즈 오천성이 대성공을 거뒀지만, 요즘 신규 사용자의 수가 급감한 것이다. 원인은 신규 사용자와 기존 사용자 사이에 스테이지 차이가 너무 큰 것이 문제였다.
+//
+//이 문제를 어떻게 할까 고민 한 그녀는 동적으로 게임 시간을 늘려서 난이도를 조절하기로 했다. 역시 슈퍼 개발자라 대부분의 로직은 쉽게 구현했지만, 실패율을 구하는 부분에서 위기에 빠지고 말았다. 오렐리를 위해 실패율을 구하는 코드를 완성하라.
+//
+//실패율은 다음과 같이 정의한다.
+//스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
+//전체 스테이지의 개수 N, 게임을 이용하는 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열 stages가 매개변수로 주어질 때, 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 return 하도록 solution 함수를 완성하라.
+//
+//제한사항
+//스테이지의 개수 N은 1 이상 500 이하의 자연수이다.
+//stages의 길이는 1 이상 200,000 이하이다.
+//stages에는 1 이상 N + 1 이하의 자연수가 담겨있다.
+//각 자연수는 사용자가 현재 도전 중인 스테이지의 번호를 나타낸다.
+//단, N + 1 은 마지막 스테이지(N 번째 스테이지) 까지 클리어 한 사용자를 나타낸다.
+//만약 실패율이 같은 스테이지가 있다면 작은 번호의 스테이지가 먼저 오도록 하면 된다.
+//스테이지에 도달한 유저가 없는 경우 해당 스테이지의 실패율은 0 으로 정의한다.
+
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+    var answer: [(Int, Float)] = []
+    var people: Float = Float(stages.count)
+    var countDic: [Int: Float] = [:]
+    
+    stages.forEach {
+        let count = countDic[$0] ?? 0
+        countDic[$0] = count + 1
+    }
+    
+    (1...N).map { i in
+        let value = Float(countDic[i] ?? 0) / people
+        
+        people -= Float(countDic[i] ?? 0)
+        answer.append((i, value))
+    }
+
+    return answer.sorted { $0.1 > $1.1 }.map({ $0.0 })
+}
+
+solution(5, [2, 1, 2, 6, 2, 4, 3, 3]) // [3,4,2,1,5]
+solution(4, [4,4,4,4,4])              // [4,1,2,3]
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
