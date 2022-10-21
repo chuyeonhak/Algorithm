bc750e6257e7ef8239996593dc4626c5f8e12a31
chuyeonhak <=>
2022-10-21 17:17:15 +0900

    작업 중

--- Programmers/Level_1/Level_1.playground/Contents.swift
+++ Programmers/Level_1/Level_1.playground/Contents.swift
@@ -1694,33 +1694,129 @@ extension Int {
 //
 //질문마다 판단하는 지표를 담은 1차원 문자열 배열 survey와 검사자가 각 질문마다 선택한 선택지를 담은 1차원 정수 배열 choices가 매개변수로 주어집니다. 이때, 검사자의 성격 유형 검사 결과를 지표 번호 순서대로 return 하도록 solution 함수를 완성해주세요.
 
-func solution(_ survey:[String], _ choices:[Int]) -> String {
-    var mbtiDic: [Character: Int] = [:]
-    var answer = ""
-    
-    zip(survey, choices).forEach {
-        switch $0.1 {
-        case 1...3:
-            mbtiDic[$0.0.first!] = (mbtiDic[$0.0.first!] ?? 0) + 4 - $0.1
-            
-        case 5...7:
-            mbtiDic[$0.0.last!] = (mbtiDic[$0.0.last!] ?? 0) + $0.1 - 4
-            
-        default: break
-        }
-    }
-    
-    (mbtiDic["R"] ?? 0) >= (mbtiDic["T"] ?? 0) ? answer.append("R") : answer.append("T")
-    (mbtiDic["C"] ?? 0) >= (mbtiDic["F"] ?? 0) ? answer.append("C") : answer.append("F")
-    (mbtiDic["J"] ?? 0) >= (mbtiDic["M"] ?? 0) ? answer.append("J") : answer.append("M")
-    (mbtiDic["A"] ?? 0) >= (mbtiDic["N"] ?? 0) ? answer.append("A") : answer.append("N")
+//func solution(_ survey:[String], _ choices:[Int]) -> String {
+//    var mbtiDic: [Character: Int] = [:]
+//    var answer = ""
+//
+//    zip(survey, choices).forEach {
+//        switch $0.1 {
+//        case 1...3:
+//            mbtiDic[$0.0.first!] = (mbtiDic[$0.0.first!] ?? 0) + 4 - $0.1
+//
+//        case 5...7:
+//            mbtiDic[$0.0.last!] = (mbtiDic[$0.0.last!] ?? 0) + $0.1 - 4
+//
+//        default: break
+//        }
+//    }
+//
+//    (mbtiDic["R"] ?? 0) >= (mbtiDic["T"] ?? 0) ? answer.append("R") : answer.append("T")
+//    (mbtiDic["C"] ?? 0) >= (mbtiDic["F"] ?? 0) ? answer.append("C") : answer.append("F")
+//    (mbtiDic["J"] ?? 0) >= (mbtiDic["M"] ?? 0) ? answer.append("J") : answer.append("M")
+//    (mbtiDic["A"] ?? 0) >= (mbtiDic["N"] ?? 0) ? answer.append("A") : answer.append("N")
+//
+//    return answer
+//}
+//
+//solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5])   // "TCMA"
+//solution(["TR", "RT", "TR"], [7, 1, 3])                     // "RCJA"
+//solution(["AN", "NA", "AN"], [4, 4, 7])                     // "RCJN"
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    return answer
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//신고 결과 받기
+//문제 설명
+//신입사원 무지는 게시판 불량 이용자를 신고하고 처리 결과를 메일로 발송하는 시스템을 개발하려 합니다. 무지가 개발하려는 시스템은 다음과 같습니다.
+//
+//각 유저는 한 번에 한 명의 유저를 신고할 수 있습니다.
+//신고 횟수에 제한은 없습니다. 서로 다른 유저를 계속해서 신고할 수 있습니다.
+//한 유저를 여러 번 신고할 수도 있지만, 동일한 유저에 대한 신고 횟수는 1회로 처리됩니다.
+//k번 이상 신고된 유저는 게시판 이용이 정지되며, 해당 유저를 신고한 모든 유저에게 정지 사실을 메일로 발송합니다.
+//유저가 신고한 모든 내용을 취합하여 마지막에 한꺼번에 게시판 이용 정지를 시키면서 정지 메일을 발송합니다.
+//다음은 전체 유저 목록이 ["muzi", "frodo", "apeach", "neo"]이고, k = 2(즉, 2번 이상 신고당하면 이용 정지)인 경우의 예시입니다.
+//
+//유저 ID    유저가 신고한 ID    설명
+//"muzi"    "frodo"    "muzi"가 "frodo"를 신고했습니다.
+//"apeach"    "frodo"    "apeach"가 "frodo"를 신고했습니다.
+//"frodo"    "neo"    "frodo"가 "neo"를 신고했습니다.
+//"muzi"    "neo"    "muzi"가 "neo"를 신고했습니다.
+//"apeach"    "muzi"    "apeach"가 "muzi"를 신고했습니다.
+//각 유저별로 신고당한 횟수는 다음과 같습니다.
+//
+//유저 ID    신고당한 횟수
+//"muzi"    1
+//"frodo"    2
+//"apeach"    0
+//"neo"    2
+//위 예시에서는 2번 이상 신고당한 "frodo"와 "neo"의 게시판 이용이 정지됩니다. 이때, 각 유저별로 신고한 아이디와 정지된 아이디를 정리하면 다음과 같습니다.
+//
+//유저 ID    유저가 신고한 ID    정지된 ID
+//"muzi"    ["frodo", "neo"]    ["frodo", "neo"]
+//"frodo"    ["neo"]    ["neo"]
+//"apeach"    ["muzi", "frodo"]    ["frodo"]
+//"neo"    없음    없음
+//따라서 "muzi"는 처리 결과 메일을 2회, "frodo"와 "apeach"는 각각 처리 결과 메일을 1회 받게 됩니다.
+//
+//이용자의 ID가 담긴 문자열 배열 id_list, 각 이용자가 신고한 이용자의 ID 정보가 담긴 문자열 배열 report, 정지 기준이 되는 신고 횟수 k가 매개변수로 주어질 때, 각 유저별로 처리 결과 메일을 받은 횟수를 배열에 담아 return 하도록 solution 함수를 완성해주세요.
+//
+//제한사항
+//2 ≤ id_list의 길이 ≤ 1,000
+//1 ≤ id_list의 원소 길이 ≤ 10
+//id_list의 원소는 이용자의 id를 나타내는 문자열이며 알파벳 소문자로만 이루어져 있습니다.
+//id_list에는 같은 아이디가 중복해서 들어있지 않습니다.
+//1 ≤ report의 길이 ≤ 200,000
+//3 ≤ report의 원소 길이 ≤ 21
+//report의 원소는 "이용자id 신고한id"형태의 문자열입니다.
+//예를 들어 "muzi frodo"의 경우 "muzi"가 "frodo"를 신고했다는 의미입니다.
+//id는 알파벳 소문자로만 이루어져 있습니다.
+//이용자id와 신고한id는 공백(스페이스)하나로 구분되어 있습니다.
+//자기 자신을 신고하는 경우는 없습니다.
+//1 ≤ k ≤ 200, k는 자연수입니다.
+//return 하는 배열은 id_list에 담긴 id 순서대로 각 유저가 받은 결과 메일 수를 담으면 됩니다.
+
+func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
+    return []
 }
 
-solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5])   // "TCMA"
-solution(["TR", "RT", "TR"], [7, 1, 3])                     // "RCJA"
-solution(["AN", "NA", "AN"], [4, 4, 7])                     // ""
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2)     // [2,1,1,0]
+solution(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3)                                          // [0,0]
 
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//func containsNearbyDuplicate(_ nums: [Int], _ k: Int) -> Bool {
+//    var dic: [Int: [Int]] = [:]
+//
+//    let _ = nums.enumerated().forEach {
+//        var value = dic[$0.element] ?? []
+//        value.append($0.offset)
+//        dic[$0.element] = value
+//    }
+//
+//    let sortedDic = dic.filter { $0.value.count >= 2 }
+//
+//    for dic in sortedDic {
+//        for i in dic.value.enumerated() {
+//            guard  dic.value.indices ~= i.offset + 1 else { continue }
+//            if abs(dic.value[i.offset] - dic.value[i.offset + 1]) <= k {
+//                return true
+//            } else { continue }
+//        }
+//    }
+//
+//    return false
+//}
+
+func containsNearbyDuplicate(_ nums: [Int], _ k: Int) -> Bool {
+        var dict = [Int: Int]()
+        for (currentIndex, num) in nums.enumerated() {
+            if let duplicateIndex = dict[num], currentIndex - duplicateIndex <= k {
+                return true
+            }
+            dict[num] = currentIndex
+        }
+        return false
+    }
+
+containsNearbyDuplicate([1,2,3,1], 3)
+containsNearbyDuplicate([1,0,1,1], 1)
+containsNearbyDuplicate([1,2,3,1,2,3], 2)
+
