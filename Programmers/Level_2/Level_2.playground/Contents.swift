d8b2a93e332ec700317bc85d1a145dd35e8d511b
chuyeonhak <chuyeonhak@nate.com>
2022-10-25 23:22:36 +0900

    N개의 최소 공배수

--- Programmers/Level_2/Level_2.playground/Contents.swift
+++ Programmers/Level_2/Level_2.playground/Contents.swift
@@ -435,26 +435,78 @@ import UIKit
 //제한사항
 //문자열의 길이 : 1,000,000이하의 자연수
 //문자열은 모두 소문자로 이루어져 있습니다.
-func solution(_ s:String) -> Int {
-    guard s.count % 2 == 0 else { return 0 }
-    
-    var stack = ""
-    
-    s.map {
-        if stack.last == $0 {
-            stack.popLast()
-        } else {
-            stack.append($0)
-        }
+//func solution(_ s:String) -> Int {
+//    guard s.count % 2 == 0 else { return 0 }
+//
+//    var stack = ""
+//
+//    s.map {
+//        if stack.last == $0 {
+//            stack.popLast()
+//        } else {
+//            stack.append($0)
+//        }
+//    }
+//
+//    return stack.isEmpty ? 1: 0
+//}
+//
+//solution("baabaa")  // 1
+//solution("cdcd")    // 0
+//solution("babbabbb")
+
+//    --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//    --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//N개의 최소공배수
+//문제 설명
+//두 수의 최소공배수(Least Common Multiple)란 입력된 두 수의 배수 중 공통이 되는 가장 작은 숫자를 의미합니다. 예를 들어 2와 7의 최소공배수는 14가 됩니다. 정의를 확장해서, n개의 수의 최소공배수는 n 개의 수들의 배수 중 공통이 되는 가장 작은 숫자가 됩니다. n개의 숫자를 담은 배열 arr이 입력되었을 때 이 수들의 최소공배수를 반환하는 함수, solution을 완성해 주세요.
+//
+//제한 사항
+//arr은 길이 1이상, 15이하인 배열입니다.
+//arr의 원소는 100 이하인 자연수입니다.
+
+//func solution(_ arr:[Int]) -> Int {
+//    var result = arr[0]
+//
+//    Array(arr.dropFirst()).map {
+//        result = ($0 * result) / getGreatestCommonDivisor($0, result)
+//    }
+//
+//    return result
+//}
+//
+func getGreatestCommonDivisor(_ m: Int, _ n: Int) -> Int {
+    var a = 0
+    var b = max(m, n)
+    var r = min(m, n)
+    while r != 0 {
+        a = b
+        b = r
+        r = a % b
     }
-    
-    return stack.isEmpty ? 1: 0
+    return b
 }
 
-solution("baabaa")  // 1
-solution("cdcd")    // 0
-solution("babbabbb")
+func gcd(_ a: Int, _ b: Int) -> Int {
+    let r = a % b
+    if r != 0 {
+        return gcd(b, r)
+    } else {
+        return b
+    }
+}
 
+func lcm(_ m: Int, _ n: Int) -> Int {
+  return m / gcd(m, n) * n
+}
+func solution(_ arr:[Int]) -> Int {
+    return arr.reduce(1) { lcm($0, $1) }
+}
+
+
+solution([2,6,8,14])        // 168
+solution([1,2,3])           // 6
 //    --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //    --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
