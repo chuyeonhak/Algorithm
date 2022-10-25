99006dd6585435f6875688ed1b58ad8afd5a79d2
chuyeonhak <=>
2022-10-25 14:50:16 +0900

    올바른 괄호

--- Programmers/Level_2/Level_2.playground/Contents.swift
+++ Programmers/Level_2/Level_2.playground/Contents.swift
@@ -34,26 +34,78 @@ import UIKit
 //숫자로만 이루어진 단어는 없습니다.
 //공백문자가 연속해서 나올 수 있습니다.
 
-func solution(_ s:String) -> String {
-    var str = s.components(separatedBy: " ")
-    var answer = ""
+//func solution(_ s:String) -> String {
+//    var str = s.components(separatedBy: " ")
+//    var answer = ""
+//
+//    for i in str {
+//        for (index, char) in i.enumerated() {
+//            if index == 0 {
+//                answer += char.uppercased()
+//            } else {
+//                answer += char.lowercased()
+//            }
+//        }
+//        answer += " "
+//    }
+//    answer.removeLast()
+//
+//    return answer
+//}
+//
+//solution("3people unFollowed me")       // "3people Unfollowed Me"
+//solution("for the last week")           // "For The Last Week"
+
+//    --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//    --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//올바른 괄호
+//문제 설명
+//괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+//
+//"()()" 또는 "(())()" 는 올바른 괄호입니다.
+//")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+//'(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+//
+//제한사항
+//문자열 s의 길이 : 100,000 이하의 자연수
+//문자열 s는 '(' 또는 ')' 로만 이루어져 있습니다.
+
+//func solution(_ s:String) -> Bool {
+//    var queue = [Character]()
+//
+//    for char in s {
+//        if char == ")" {
+//            guard !queue.isEmpty else { return false }
+//            queue.removeFirst()
+//        } else {
+//            queue.insert(char, at: 0)
+//        }
+//    }
+//
+//    return queue.isEmpty
+//}
+
+func solution(_ s:String) -> Bool {
+    var stack = [Character]()
     
-    for i in str {
-        for (index, char) in i.enumerated() {
-            if index == 0 {
-                answer += char.uppercased()
-            } else {
-                answer += char.lowercased()
-            }
+    for char in s {
+        if char == "(" {
+            stack.append(char)
+        } else {
+            guard !stack.isEmpty else { return false }
+            stack.popLast()
         }
-        answer += " "
     }
-    answer.removeLast()
     
-    return answer
+    return stack.isEmpty
 }
 
-solution("3people unFollowed me")       // "3people Unfollowed Me"
-solution("for the last week")           // "For The Last Week"
+solution("()()")       // true
+solution("(())()")     // true
+solution(")()(")       // false
+solution("(()(")       // false
     
-    
+//    --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//    --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
