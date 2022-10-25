dfd9025529ea8eefa3eca3368b82400a36dc8976
chuyeonhak <=>
2022-10-25 18:41:10 +0900

    카펫 미완

--- Programmers/Level_2/Level_2.playground/Contents.swift
+++ Programmers/Level_2/Level_2.playground/Contents.swift
@@ -288,17 +288,64 @@ import UIKit
 //    return answer
 //}
 
-func solution(_ n:Int) -> Int {
-    var answer : Int = n + 1
+//func solution(_ n:Int) -> Int {
+//    var answer : Int = n + 1
+//
+//    while true {
+//        print(n.nonzeroBitCount)
+//        if n.nonzeroBitCount == answer.nonzeroBitCount { break }
+//        answer += 1
+//    }
+//
+//    return answer
+//}
+//
+//solution(78) // 83
+//solution(15) // 23
+//    --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    while true {
-        print(n.nonzeroBitCount)
-        if n.nonzeroBitCount == answer.nonzeroBitCount { break }
-        answer += 1
+//    --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//카펫
+//문제 설명
+//Leo는 카펫을 사러 갔다가 아래 그림과 같이 중앙에는 노란색으로 칠해져 있고 테두리 1줄은 갈색으로 칠해져 있는 격자 모양 카펫을 봤습니다.
+//
+//Leo는 집으로 돌아와서 아까 본 카펫의 노란색과 갈색으로 색칠된 격자의 개수는 기억했지만, 전체 카펫의 크기는 기억하지 못했습니다.
+//
+//Leo가 본 카펫에서 갈색 격자의 수 brown, 노란색 격자의 수 yellow가 매개변수로 주어질 때 카펫의 가로, 세로 크기를 순서대로 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+//
+//제한사항
+//갈색 격자의 수 brown은 8 이상 5,000 이하인 자연수입니다.
+//노란색 격자의 수 yellow는 1 이상 2,000,000 이하인 자연수입니다.
+//카펫의 가로 길이는 세로 길이와 같거나, 세로 길이보다 깁니다.
+
+func solution(_ brown:Int, _ yellow:Int) -> [Int] {
+    let sum = brown + yellow
+    var tempArr: [Int] = []
+    
+    let range = brown / yellow > yellow ? (yellow...(brown / yellow)): ((brown / yellow)...yellow)
+    for i in range {
+        if sum % i == 0 {
+            tempArr.append(i)
+        }
     }
-
-    return answer
+    
+    for i in tempArr {
+        if i != yellow {
+            print("한쪽 == \(sum / i), 다른쪽 == \(i)")
+        }
+    }
+    
+    switch tempArr.count {
+    case 1: return [tempArr[0], tempArr[0]]
+    case 2: return [tempArr.max()!, tempArr.min()!]
+    default: return [1, 3]
+    }
 }
 
-solution(78) // 83
-solution(15) // 23
+solution(10, 2)         // [4, 3]
+solution(8, 1)          // [3, 3]
+solution(24, 24)        // [8, 6]
+solution(12, 3)           // [5, 3]
+//    --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//    --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
