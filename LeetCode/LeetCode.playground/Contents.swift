396873ed9782fb86cf0020cc908a665a87cc0c6c
chuyeonhak <=>
2022-11-01 08:40:24 +0900

    Permutation in String

--- LeetCode/LeetCode.playground/Contents.swift
+++ LeetCode/LeetCode.playground/Contents.swift
@@ -635,30 +635,57 @@ class Solution {
 //    }
     
 //102. Binary Tree Level Order Traversal
-    func levelOrder(_ root: TreeNode?) -> [[Int]] {
-            guard root != nil else { return [] }
-            var result: [[Int]] = []
-            
-            var queue: [TreeNode?] = []
-            queue.append(root)
-            
-            while(!queue.isEmpty) {
-                var count = queue.count - 1
-                var temp: [Int] = []
-                 while(count >= 0) {
-                   if let node = queue.removeFirst() {
-                       temp.append(node.val)
-                       count -= 1
-                       if let left = node.left { queue.append(left) }
-                       if let right = node.right { queue.append(right) }
-                   }
-                }
-                
-                result.append(temp)
+//    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+//            guard root != nil else { return [] }
+//            var result: [[Int]] = []
+//
+//            var queue: [TreeNode?] = []
+//            queue.append(root)
+//
+//            while(!queue.isEmpty) {
+//                var count = queue.count - 1
+//                var temp: [Int] = []
+//                 while(count >= 0) {
+//                   if let node = queue.removeFirst() {
+//                       temp.append(node.val)
+//                       count -= 1
+//                       if let left = node.left { queue.append(left) }
+//                       if let right = node.right { queue.append(right) }
+//                   }
+//                }
+//
+//                result.append(temp)
+//            }
+//
+//            return result
+//        }
+    
+//567. Permutation in String
+    func checkInclusion(_ s1: String, _ s2: String) -> Bool {
+        typealias DCI = [Character:Int]
+        let as2 = Array(s2)
+        var lhs = 0, rhs = 0, match = 0, need = DCI(), win = DCI()
+        for ch in s1 { need[ch, default: 0] += 1 }
+        while rhs < as2.count {
+            let a2r = as2[rhs]
+            rhs += 1
+            if let n2r = need[a2r] {
+                win[a2r, default: 0] += 1
+                if win[a2r] == n2r { match += 1 }
             }
-            
-            return result
-        }}
+            while match == need.count {
+                if rhs - lhs == s1.count { return true }
+                let a2l = as2[lhs]
+                lhs += 1
+                if let n2l = need[a2l] {
+                    if win[a2l] == n2l { match -= 1 }
+                    win[a2l, default: 0] -= 1
+                }
+            }
+        }
+        return false
+    }
+}
 
 
 let solution = Solution()
@@ -831,7 +858,8 @@ let listNode1 = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
 //solution.preorder(node1)
 
 //102. Binary Tree Level Order Traversal
-solution.levelOrder(nil)
+//solution.levelOrder(nil)
 
-var tempArr = [Int]()
-
+//567. Permutation in String
+solution.checkInclusion("ab", "eidbaooo")
+solution.checkInclusion("ab", "eidboaoo")
